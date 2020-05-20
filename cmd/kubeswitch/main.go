f323eccaf102ec6b44f9ac50e666370f21bbb2b0
Danny Burke <danny@steamhaus.co.uk>
2020-05-20 22:06:37 +0100

    Try and get all versions WIP

--- cmd/kubeswitch/main.go
+++ cmd/kubeswitch/main.go
@@ -5,12 +5,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 	/*
-		  "os"
-		  "io"
-		  "path/filepath"
-			"regexp"
-		  "string"
+		"os"
+		"io"
+		"path/filepath"
+		"regexp"
+		"string"
 	*/)
 
 type kubeVersion struct {
@@ -23,7 +25,7 @@ func main() {
 	resp, err := http.Get("https://storage.googleapis.com/kubernetes-release/release/stable.txt")
 
 	if err != nil {
-		fmt.Println("Cannot read from remote repository")
+		fmt.Println("Cannot read from remote repository", err)
 	}
 
 	defer resp.Body.Close()
@@ -36,4 +38,36 @@ func main() {
 
 	result := string(body)
 	fmt.Println("Latest stable release is:" + " " + result)
+	getAllReleases()
+}
+
+func getAllReleases() {
+	var kubeVersion kubeVersion
+	releaseURL := "https://github.com/kubernetes/kubernetes/tags"
+
+	resp, err := http.Get(releaseURL)
+
+	if err != nil {
+		fmt.Println("Cannot see all latest releaes", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	releases := string(body)
+	result := strings.Split(releases, "\n")
+	for i := range result {
+		r, _ := regexp.Compile(`\/(\d+)(\.)(\d+)(\.)(\d+)\/`)
+
+		if r.MatchString(result[i]) {
+
+			str := r.FindString(result[i])
+			trimstr := strings.Trim(str, "tag/")
+
+			kubeVersion.version = trimstr
+			kubeVersion.url = releaseURL + trimstr
+		}
+
+		fmt.Println("All of the latest releases are:"+" ", kubeVersion)
+	}
 }
