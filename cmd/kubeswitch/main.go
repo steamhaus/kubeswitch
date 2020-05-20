e76a86c81c86216bc37812455a3ef9099d79c14c
Danny Burke <danny@steamhaus.co.uk>
2020-05-20 21:31:47 +0100

    Add struct for kube versions and print latest release

--- cmd/kubeswitch/main.go
+++ cmd/kubeswitch/main.go
@@ -1,35 +1,39 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "io/ioutil"
-  "log"
-  /*
-  "os"
-  "io"
-  "path/filepath"
-	"regexp"
-  "string"
-  */
-)
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	/*
+		  "os"
+		  "io"
+		  "path/filepath"
+			"regexp"
+		  "string"
+	*/)
+
+type kubeVersion struct {
+	version string
+	url     string
+}
 
 func main() {
 
-  resp, err := http.Get("https://storage.googleapis.com/kubernetes-release/release/stable.txt")
+	resp, err := http.Get("https://storage.googleapis.com/kubernetes-release/release/stable.txt")
 
-  if err != nil {
+	if err != nil {
 		fmt.Println("Cannot read from remote repository")
 	}
 
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 
-  if err != nil {
+	if err != nil {
 		log.Printf("Error reading body: %v", err)
 		return
 	}
 
-  result := string(body)
-  fmt.Println(result)
+	result := string(body)
+	fmt.Println("Latest stable release is:" + " " + result)
 }
