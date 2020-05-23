4cc739eefae101716e5339edeeb7da7555d4b54e
Danny Burke <danny@steamhaus.co.uk>
2020-05-23 15:14:32 +0100

    Add response to struct and get values from there

--- cmd/kubeswitch/main.go
+++ cmd/kubeswitch/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
-	"strings"
 	/*
 		"os"
 		"io"
@@ -18,6 +17,79 @@ import (
 type kubeVersion struct {
 	version string
 	url     string
+}
+
+type Releases []struct {
+	// URL             string `json:"url"`
+	// AssetsURL       string `json:"assets_url"`
+	// UploadURL       string `json:"upload_url"`
+	// HTMLURL         string `json:"html_url"`
+	// ID              int    `json:"id"`
+	// NodeID          string `json:"node_id"`
+	TagName string `json:"tag_name"`
+	// TargetCommitish string `json:"target_commitish"`
+	Name string `json:"name"`
+	// Draft           bool   `json:"draft"`
+	// Author          struct {
+	// 	Login             string `json:"login"`
+	// 	ID                int    `json:"id"`
+	// 	NodeID            string `json:"node_id"`
+	// 	AvatarURL         string `json:"avatar_url"`
+	// 	GravatarID        string `json:"gravatar_id"`
+	// 	URL               string `json:"url"`
+	// 	HTMLURL           string `json:"html_url"`
+	// 	FollowersURL      string `json:"followers_url"`
+	// 	FollowingURL      string `json:"following_url"`
+	// 	GistsURL          string `json:"gists_url"`
+	// 	StarredURL        string `json:"starred_url"`
+	// 	SubscriptionsURL  string `json:"subscriptions_url"`
+	// 	OrganizationsURL  string `json:"organizations_url"`
+	// 	ReposURL          string `json:"repos_url"`
+	// 	EventsURL         string `json:"events_url"`
+	// 	ReceivedEventsURL string `json:"received_events_url"`
+	// 	Type              string `json:"type"`
+	// 	SiteAdmin         bool   `json:"site_admin"`
+	// } `json:"author"`
+	// Prerelease  bool      `json:"prerelease"`
+	// CreatedAt   time.Time `json:"created_at"`
+	// PublishedAt time.Time `json:"published_at"`
+	// Assets      []struct {
+	// 	URL      string `json:"url"`
+	// 	ID       int    `json:"id"`
+	// 	// NodeID   string `json:"node_id"`
+	// 	Name     string `json:"name"`
+	// Label    string `json:"label"`
+	// Uploader struct {
+	// 	Login             string `json:"login"`
+	// 	ID                int    `json:"id"`
+	// 	NodeID            string `json:"node_id"`
+	// 	AvatarURL         string `json:"avatar_url"`
+	// 	GravatarID        string `json:"gravatar_id"`
+	// 	URL               string `json:"url"`
+	// 	HTMLURL           string `json:"html_url"`
+	// 	FollowersURL      string `json:"followers_url"`
+	// 	FollowingURL      string `json:"following_url"`
+	// 	GistsURL          string `json:"gists_url"`
+	// 	StarredURL        string `json:"starred_url"`
+	// 	SubscriptionsURL  string `json:"subscriptions_url"`
+	// 	OrganizationsURL  string `json:"organizations_url"`
+	// 	ReposURL          string `json:"repos_url"`
+	// 	EventsURL         string `json:"events_url"`
+	// 	ReceivedEventsURL string `json:"received_events_url"`
+	// 	Type              string `json:"type"`
+	// 	SiteAdmin         bool   `json:"site_admin"`
+	// } `json:"uploader"`
+	// 	ContentType        string    `json:"content_type"`
+	// 	State              string    `json:"state"`
+	// 	Size               int       `json:"size"`
+	// 	DownloadCount      int       `json:"download_count"`
+	// 	CreatedAt          time.Time `json:"created_at"`
+	// 	UpdatedAt          time.Time `json:"updated_at"`
+	// 	//BrowserDownloadURL string    `json:"browser_download_url"`
+	// } `json:"assets"`
+	// //TarballURL string `json:"tarball_url"`
+	// ZipballURL string `json:"zipball_url"`
+	// //Body       string `json:"body"`
 }
 
 func main() {
@@ -42,32 +114,19 @@ func main() {
 }
 
 func getAllReleases() {
-	var kubeVersion kubeVersion
-	releaseURL := "https://github.com/kubernetes/kubernetes/tags"
+	releaseURL := "https://api.github.com/repos/kubernetes/kubernetes/releases"
 
 	resp, err := http.Get(releaseURL)
 
 	if err != nil {
-		fmt.Println("Cannot see all latest releaes", err)
+		fmt.Println("Cannot see all latest releaes", err, http.StatusInternalServerError)
 	}
 
-	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-
-	releases := string(body)
-	result := strings.Split(releases, "\n")
-	for i := range result {
-		r, _ := regexp.Compile(`\/(\d+)(\.)(\d+)(\.)(\d+)\/`)
-
-		if r.MatchString(result[i]) {
-
-			str := r.FindString(result[i])
-			trimstr := strings.Trim(str, "tag/")
-
-			kubeVersion.version = trimstr
-			kubeVersion.url = releaseURL + trimstr
-		}
-
-		fmt.Println("All of the latest releases are:"+" ", kubeVersion)
+	if err != nil {
+		fmt.Errorf("Read body: %v", err)
 	}
+	var data Releases
+	json.Unmarshal(body, &data)
+	fmt.Printf("Other results are: %v\n", data)
 }
