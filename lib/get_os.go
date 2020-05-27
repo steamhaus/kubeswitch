f85532ea8863e51b07c8542df40da26d3ec390ea
Danny Burke <danny@steamhaus.co.uk>
2020-05-27 23:04:02 +0100

    Remove redundant struct for now

--- lib/get_os.go
+++ lib/get_os.go
@@ -4,16 +4,6 @@ import (
 	"github.com/matishsiao/goInfo"
 )
 
-type GoInfoObject struct {
-	GoOS     string
-	Kernel   string
-	Core     string
-	Platform string
-	OS       string
-	Hostname string
-	CPUs     int
-}
-
 func main() {
 	gi := goInfo.GetInfo()
 	gi.VarDump()
